40a76b4d6d28bcc5bf8dcf152e8aceccbede6265
CrazyUP <92596340+CrazuUP@users.noreply.github.com>
2024-11-08 06:05:17 +0600

    Update lab1.bash

--- lab1.bash
+++ lab1.bash
@@ -1,50 +1,52 @@
 #!/bin/bash
 
-echo "Создаём файлы "
-touch amoonguss4 dewott7
+  GNU nano 6.2                                                                                        lab1.bash *                                                                                                #создание папки со всеми файлами
+mkdir labo
+cd lab0
 
-mkdir escavalier1/numel escavalier1/blitzle escavalier1/sandshrew escavalier1/loudred
-touch escavalier1/tentacool escavalier1/slowpoke
+#Создаём файлы и папки каталога
+touch amoonguss4/dewott7/leafeon4
+echo Ходы After  You Foul Play Gastro Acid Giga Drain Seed Bomb Sleep Talk Snore Synthesis Worry Seed > ammonguss4
+echo Развитые способности Shell Armor > dewott7
+echo Тип покемона GRASS NONE > leafeon4
 
-mkdir golem8/bronzong golem8/archen golem8/gothita
-touch golem8/snivy golem8/charizard
+mkdir escavalier1
+cd escavalier1
+mkdir numel/blitzle/sandshrew/loudred
+touch tentacool/slowpoke
+echo satk=5 sdef=10 spd=7 > tentacool
+echo satk=5 sdef=4 spd=2 > slowpoke
 
-mkdir weepinbell8/rufflet weepinbell8/whiskcash
-touch weepinbell8/vulpex
+#возвращаемся в корневую
+cd ..
 
-echo "Записываем информацию в файлы"
-echo "Ходы After You Foul Play Gastro Acid Giga Drain Seed Bomb Sleep Talk Snore Synthesis" > amoonguss4
-echo "Развитые способности Shell Armor" > dewott7
-echo "Satk=5 sdef=10 spd=7" > escavalier1/tentacool
-echo "satk=4 sdef=4 spd=2" > escavalier1/slowpoke
-echo "Способности Takle Leer Vine Wrap Growth Leaf Tornado Leech Seed Mega Drain Slam Leaf Blade Coil Drain Wring Out Gastro Acid Leaf" > escavalier1/snivy
-echo "ТУТ НИЧЕГО НЕТ" > golem8/snivy
-echo "Ходы Air Cutter Air Slash Blast Burn Body Slam Counter Defense Curl Double-Edge Dragon Clawl Dragon Pulse Dynamicpunch Fire Pledge!" > charizard
-echo "Тип покемона Grass NONE" > leafeon4
-echo "Её всё ещё нет...." > whiskcash
-echo "Тип диеты Herbivore" > vulpix
+#создаём golem8
+mkdir goleam8
+cd goleam8
+mkdir bronzong/archen/gothita
+touch snivy/charizard
+echo Способности Tackle Leer Vine Whip Wrap Growth Leaf Tornado Leech Seed Mega Drain Slam Leaf Blade Coil Giga Drain Wring Out Gastro Acid Leaf Storm > snivy
+echo Ходы Air Cutter Air Slasht Blast Burn Body Slam Counter Defense Curl Double-Edge Dragon Claw+ Dragon Pulse Dynamicpunch Fire Pledge Fire Punch Focus Punch Fury Cutter Growl Heat Wave Iron Tail Mega Kick >
 
-echo "Выдаём права доступа"
-chmod 600 escavalier1/tentacool
+#возвращаемся в корневую
+cd ..
+
+#создаём weepinbell8
+mkdir weepinbell8
+cd weepinbell8
+mkdir rufflet/whiscash
+touch vulpix
+echo Тип диеты Herbivore > vulpix
+
+#возвращаемся в корневую
+cd ..
+
+#выдаём права доступа
+
+chmod 604 amoonguss4
 chmod 444 dewott7
-chmod 700 escavalier1
-chmod 771 escavalier1/numel
-chmod 111 escavalier1/blitzle
-chmod 400 escavalier1/slowpoke
-chmod 770 escavalier1/sandshrew
-chmod 511 escavalier1/loudred
-chmod 200 escavalier1/slowpoke
-chmod 775 golem8
-chmod 444 golem8/snivy
-chmod 571 golem8/bronzong
-chmod 111 golem8/archen
-chmod 555 golem8/gothita
-chmod 666 golem8/charizard
-chmod 200 golem8/leafeon4
-chmod 357 weepinbell8
-chmod 777 weepinbell8/rufflet
-chmod 337 weepinbell8/whiskcash
-chmod 400 weepinbell8/vulpix
+chmod
+
 
 rm -r /lab0/escavalier1/blitzle
 cp -R /path/to/escavalier1 /path/to/lab0/escavalier1/numel
